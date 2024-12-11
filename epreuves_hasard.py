2a3f52a9a6907844a85a6a1f06f7f5dc09a25ca3
Nicolas <nicolas.albadoro@gmail.com>
2024-12-11 10:54:40 +0100

    Début de epreuves_hasard

---
+++ epreuves_hasard.py
@@ -0,0 +1,13 @@
+from random import*
+
+def bonneteau():
+    Liste = ['A','B','C']
+    BonneLettre = str()
+    Choix = str()
+    print("Bienvenue sur le jeu Bonneteau !\nLes règles de ce jeu sont : \nLe joueur doit deviner sous quel bonneteau (A, B ou C) se cache la clé. Il dispose de deux essais pour le trouver. À chaque essai, la clé est placée aléatoirement sous l'un des bonneteaux.")
+    print("\nLes bonneteaux disponibles sont : le A, le B et le C")
+    for i in range(2):
+        BonneLettre = choice(Liste)
+        print("Il vous reste :",i+1,"tentatives !")
+        print("Saississez un bonneteau entre le A, le B et le C : ")
+        Choix = 
