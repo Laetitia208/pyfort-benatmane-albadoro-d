a208863c419cf842fc12ddb93882c43609db8035
laetitia <laetitiabenatmane@gmail.com>
2024-12-19 15:27:49 +0100

    maths fini

--- epreuves_mathematiques.py
+++ epreuves_mathematiques.py
@@ -1,59 +1,61 @@
 from pydoc import importfile
 
-def factorielle(n):
+def factorielle(n):                                                   #Ce programme calcule la factorielle du nombre n
     f = 1
     if n == 0:
-        return 1
-    for i in range(1, n+1):
+        return 1                                                      #0! = 1
+    for i in range(1, n+1):                                           #On multiplie f par tous les termes de 1 à n+1
         f = f*i
     return f
 
 import random
 
-def epreuve_math_factorielle():
+def epreuve_math_factorielle():                                       #Ce programme est une épreuve pour trouver la factorielle d'un nombre entre 1 et 10
     n = random.randint(1, 10)
     print("Epreuve de Mathématiques : calculer la factorielle de", n)
-    joueur = int(input())
+    joueur = int(input())                                             #Le joueur entre sa réponse
     print("Votre réponse :", joueur)
-    reponse = factorielle(n)
+    reponse = factorielle(n)                                          #Le programme utilise la fonction factorielle(n) pour calculer n!
     if reponse == joueur :
         print("Correct ! Vous gagnez une clé.")
-        return True
+        return True                                                   #Si c'est correcte, la fonction retourne True
     else:
-        return False
+        print("Dommage, mauvaise réponse.")
+        return False                                                  #Sinon elle retourne False
 
-def est_premier(n):
+def est_premier(n):                                                   #Cette fonction  vérifie si un nombre n est premier
     pr = True
     if n <= 1 :
-        return False
-    for i in range(2, n):
+        return False                                                  #Si n<=1, alors n n'est pas premier
+    for i in range(2, n):                                             #On vérifie pour toutes les valeurs inférieures à n (2 à n-1) s'il y a des diviseurs de n
         if n % i == 0:
             pr = False
     return pr
 
-def premier_plus_proche(n):
+def premier_plus_proche(n):                                           #Cette fonction trouve le nombre premier sépérieur ou égal à n le plus proche
     proche = False
     i = n
     while proche == False :
-        if est_premier(i) == True:
+        if est_premier(i) == True:                                    #Si i est permier, la boucle se ferme et la fonction retourne i
             proche = True
             return i
         else:
-            i = i + 1
+            i = i + 1                                                 #Sinon on ajoute 1 à i tant que i n'est pas premier
 
 def epreuve_math_premier():
     n = random.randint(10, 20)
-    print("Epreuve de Mathématiques : trouver le nombre premier le plus proche de", n)
+    print("Epreuve de Mathématiques : trouver le nombre premier supérieur ou égal le plus proche de", n)
     joueur = int(input())
     print("Votre réponse :", joueur)
-    reponse = premier_plus_proche(n)
-    if reponse == joueur :
+    reponse = premier_plus_proche(n)                                  #Le programme utilise la fonction premier_plus_proche() pour n
+    if reponse == joueur :                                            #Ensuite, il compare la réponse du joueur à la bonne réponse
         print("Correct ! Vous gagnez une clé.")
         return True
     else:
+        print("Dommage, mauvaise réponse.")
         return False
 
-def epreuve_roulette_mathematique():
+def epreuve_roulette_mathematique():                                  #Ce programme génère un calcul aléatoire et demande au joueur de toruver le résultat
     nb1 = random.randint(1, 20)
     nb2 = random.randint(1, 20)
     nb3 = random.randint(1, 20)
@@ -82,6 +84,7 @@ def epreuve_roulette_mathematique():
         print("Bonne réponse ! Vous avez gagné une clé.")
         return True
     else:
+        print("Dommage, mauvaise réponse.")
         return False
 
 def epreuve_math():
