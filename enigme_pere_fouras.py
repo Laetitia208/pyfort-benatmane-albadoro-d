8c71e4be89a05706e75ad1aa0d1295ba33cf131d
laetitia <laetitiabenatmane@gmail.com>
2025-01-01 16:24:40 +0100

    Terminé(annoter)

--- enigme_pere_fouras.py
+++ enigme_pere_fouras.py
@@ -1,2 +1,38 @@
+import json
+
 def charger_enigmes(fichier):
-    enigmes = open(fichier, 'r')
+    with open(fichier,'r',encoding='utf-8') as f:
+        enigmes = json.load(f)
+    d = {}
+    for enigme in enigmes:
+        d[enigme['question']] = enigme['reponse']
+    return d
+
+print(charger_enigmes('./data/enigmesPF.json'))
+
+import random
+
+def enigme_pere_fouras():
+    nb_essais = 3
+    liste_enigmes = (charger_enigmes('./data/enigmesPF.json'))
+    enigme_choisie,rep = random.choice(list(liste_enigmes.items()))
+    print(enigme_choisie)
+    while nb_essais > 0:
+        print("Saisissez une réponse :")
+        reponse = input()
+        reponse_min = reponse.lower()
+        rep_min = rep.lower()
+        if reponse_min == rep or reponse == rep or reponse_min == rep_min or reponse == rep_min:
+            print("Félicitation, votre réponse esr correcte ! Vous avez gagné une clé")
+            return True
+        else:
+            nb_essais -= 1
+            if nb_essais > 1:
+                print("Malheureusement, votre réponse est incorrecte. Il vous reste",nb_essais,"essais.")
+            if nb_essais == 1:
+                print("Malheureusement, votre réponse est incorrecte. Il vous reste", nb_essais, "essai.")
+            if nb_essais == 0:
+                print("Dommage, vous avez échoué à cette épreuve. La réponse était :",rep)
+                return False
+
+enigme_pere_fouras()
